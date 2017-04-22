d1f98614a5d1a0f5f3df9798fdc6f6a18304f9ba
phpacad <git@php-academy.kiev.ua>
2017-04-22 13:36:36 +0300

    Added print VS echo demonstration

---
+++ samples/print.php
@@ -0,0 +1,12 @@
+<?php
+
+$result = print('test');
+var_dump($result);
+
+echo 'qwerty' . PHP_EOL;
+
+echo 'qwe', 'test', 'some string', 123, PHP_EOL;
+
+print_r(['test', 123]);
+
+printf('test %s qwerty %f', 'PHP', 7.1);
