b4c3facf2d62e9c0d1b675e95b6e80040707f7e2
phpacad <git@php-academy.kiev.ua>
2017-04-22 13:44:56 +0300

    Added strings demonstration

--- samples/print.php
+++ samples/print.php
@@ -10,3 +10,15 @@ echo 'qwe', 'test', 'some string', 123, PHP_EOL;
 print_r(['test', 123]);
 
 printf('test %s qwerty %f', 'PHP', 7.1);
+
+echo PHP_EOL;
+
+$q1 = 'some string';
+var_dump('=== $q1 ===');
+var_dump("=== {$q1} ===");
+$js = <<<JS
+console.log('test');
+JS;
+echo $js . PHP_EOL;
+
+
