20567fb9e991baf7e956862381b86db170f4ee8d
phpacad <git@php-academy.kiev.ua>
2017-04-29 12:20:45 +0300

    Added errors demonstration

--- samples/errors.php
+++ samples/errors.php
@@ -3,11 +3,10 @@
 error_reporting(E_ALL);
 //ini_set('error_reporting', E_ALL);
 
-1 / 0;
-echo $test + 1;
+//1 / 0;
+//echo $test + 1;
 
-echo 123;
-
+//echo 123;
 ?>
 
 <?php header('Location: /') ?>
