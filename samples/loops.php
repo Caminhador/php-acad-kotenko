de0e00af28aa7fc59beb6d1151df11c177ba67c4
phpacad <git@php-academy.kiev.ua>
2017-04-29 12:56:27 +0300

    Added loops demonstration

--- samples/loops.php
+++ samples/loops.php
@@ -32,3 +32,27 @@ $values = [
 foreach ($values['test'] as $key => $value) {
     echo $key . ' => ' . $value . PHP_EOL;
 }
+
+echo PHP_EOL;
+
+for ($i = 0; $i < 10; $i++) {
+    if ($i == 5) {
+        continue;
+    }
+
+    echo $i;
+}
+
+echo PHP_EOL;
+
+for ($i = 0; $i < 10; $i++) {
+    if ($i == 5) {
+        break;
+    }
+
+    echo $i;
+}
+
+die('Died');
+//die('Exited');
+echo 'Yeah';
