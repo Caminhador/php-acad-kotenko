4760d09de73b0d74cf5389eb61c5835e08942999
Taras Shkodenko <taras@shkodenko.com>
2017-05-17 20:56:04 +0300

    Added files upload examples

---
+++ samples/tasks/files-upload-app/index.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+
+$config = require_once __DIR__ . '/configs/main.php';
+
+require_once __DIR__ . '/lib/arrays.php';
+
+require_once __DIR__ . '/form.php';
+require_once __DIR__ . '/file_actions.php';
