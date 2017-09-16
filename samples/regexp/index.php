5d77167d2bfa2d33db634118ba23f38c76f4734e
phpacad <git@php-academy.kiev.ua>
2017-09-16 12:59:43 +0300

    Added extended regexp examples

--- samples/regexp/index.php
+++ samples/regexp/index.php
@@ -3,20 +3,26 @@
 $string = '[abc]	A single character of: a, b or c
 [^abc]	Any single character except: a, b, or c
 [a-z]	Any single character in the range a-z
-[a-zA-Z]	Any single character in the range a-z or A-Z
+[a-zA-Z]	Any single character in the range a-z or A-ZD
 ^	Start of line
 $	End of line
 \A	Start of string
 \z	End of string';
 
-echo '<pre>', $string, '</pre>';
+$pattern = '/^\[(.*)\](.*)([A-Z]{2})$/m';
 
-preg_match('/\[[a-z]\s?\-[a-z][A-Z]\-[A-Z]\]/', $string, $matches);
-var_dump($matches);
+echo '<pre>', $string, '<hr>';
 
-preg_match_all('/Any/', $string, $matches);
-var_dump($matches);
+echo 'Pattern: ', $pattern;
 
+echo '<hr> PREG_MATCH:<br>';
 
+preg_match($pattern, $string, $matches);
+print_r($matches);
 
+echo '<hr> PREG_MATCH_ALL:<br>';
 
+preg_match_all($pattern, $string, $matches);
+print_r($matches);
+
+echo '</pre>';
